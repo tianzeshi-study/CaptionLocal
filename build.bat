96160548095df03f4b13d1ca041ed0fe2131133a
tianzeshi <tianzeshi_study@outlook.com>
2026-04-18 16:41:45 +0800

    update ci

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 pip install --upgrade pip wheel
 pip install -r requirements.txt
-pip install -r requirements-libs.txt --target ./addon/globalPlugins/CaptionLocal/libs --platform win32 --only-binary=:all: --no-binary=:none:
-pip install miniqinference[cli] --target ./addon/globalPlugins/CaptionLocal/libs
+pip install -r requirements-libs.txt --target ./addon/globalPlugins/CaptionLocal/libs --platform win32 --only-binary=:all: --no-binary=:none: --upgrade
+pip install miniqinference[cli] --target ./addon/globalPlugins/CaptionLocal/libs --upgrade
 
 set SKIP="no-commit-to-branch"
 pre-commit run --all
